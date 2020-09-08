8289dddcc83e6a0c228ad71e7b9c30c861ce1ebd
Hectopascal7 <hectopascal7@yeah.net>
2020-09-08 14:00:34 +0800

    项目初始化。

---
+++ src/main/java/com/mint/marsos/MarsosApplication.java
@@ -0,0 +1,13 @@
+package com.mint.marsos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsosApplication.class, args);
+    }
+
+}
